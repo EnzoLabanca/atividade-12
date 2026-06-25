64984cb64817f57603951e9821c707b609c2523d
EnzoLabanca <0001271687@senaimgaluno.com.br>
2026-06-25 16:21:30 -0300

    Create 02.js

---
+++ 02.js
@@ -0,0 +1,8 @@
+let idade = Number(prompt("qual a sua idade"));
+ if(idade >= 18){
+  console.log("acesso liberado: maior de idade");
+ }
+ else if (idade <18){
+  console.log("acesso negado: menor de idade");
+ }
+ 
