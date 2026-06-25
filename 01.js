3462d10d21f4950eee61455d1afe9802ecd45feb
EnzoLabanca <0001271687@senaimgaluno.com.br>
2026-06-25 16:09:53 -0300

    Create 01.js

---
+++ 01.js
@@ -0,0 +1,10 @@
+let numero = Number(prompt("digite um numero: "));
+if(numero > 0){
+    console.log("o numero e positivo")
+    }
+else if(numero < 0){
+    console.log("o numero e negativo")
+}
+else{ 
+    console.log("o numero e exatamente igual a zero ")
+}
