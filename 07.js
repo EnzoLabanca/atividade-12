df3fbc6065fce00e607b77191ed211fdde9129f9
EnzoLabanca <0001271687@senaimgaluno.com.br>
2026-07-01 16:36:08 -0300

    Create 07.js

---
+++ 07.js
@@ -0,0 +1,12 @@
+let nota = (prompt("qual a nota do studante"))
+
+if (nota >=7){
+    console.log("aprovado")
+}
+
+else if ((nota >=5 && nota<7)){
+    console.log("recuperacao")
+}
+else 
+    console.log("reprovado")
+
