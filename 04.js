b620018d62f32187370fd58a62918e5c646584aa
EnzoLabanca <0001271687@senaimgaluno.com.br>
2026-07-01 15:19:59 -0300

    Create 04.js

---
+++ 04.js
@@ -0,0 +1,13 @@
+let num1 = Number(prompt("digite o 1° numero"));
+let num2 = Number(prompt("digite o 2° numero"));
+if(num1 > num2){
+ console.log("o primeiro numero e maior que o segundo");
+}
+
+ else if(num1 == num2){
+ console.log("o primeiro e o segundo numero sao iguais");
+ }
+ 
+ else {
+ console.log("o segundo numero e maior que o primeiro");
+}
