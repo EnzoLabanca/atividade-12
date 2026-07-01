a76d0cfe321003e69ea215b85b44bf8a5561f188
EnzoLabanca <0001271687@senaimgaluno.com.br>
2026-07-01 16:07:24 -0300

    Create 06.js

---
+++ 06.js
@@ -0,0 +1,9 @@
+let estudante = (prompt("voce e estudante?"));
+let idade = (prompt("qual a sua idade?"));
+if ((estudante == "sim") || (idade >= 60)){
+    console.log("voce tem direito ao desconto da meia-entrada ");
+}
+else { 
+    console.log("voce nao tem direito a meia-entrada");
+}
+
